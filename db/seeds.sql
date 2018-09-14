cc4414710981b74a33d083df64c5f2dd040fac66
larry.moua <lkmoua@gmail.com>
2018-09-13 19:45:17 -0700

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Cheese');
+INSERT INTO burger (name) VALUES ('Bacon');
+INSERT INTO burger (name, devoured) VALUES ('Double Bacon', true);
+INSERT INTO burger (name, devoured) VALUES ('Low Carb', true);
+INSERT INTO burger (name, devoured) VALUES ('Double Stack', true);
+INSERT INTO burger (name) VALUES ('Spicy Chicken');
